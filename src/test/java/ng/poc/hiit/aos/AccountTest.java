035c773d71bd31dc8d4cb52988f6e09a2d30dbe0
Stephan Asemota <stephan.asemota@gmail.com>
2015-05-07 11:50:28 +0200

    removed tests, trying out ejb3unit

---
+++ src/test/java/ng/poc/hiit/aos/AccountTest.java
@@ -0,0 +1,35 @@
+package ng.poc.hiit.aos;
+
+//switched test implimentations tryingout ejb3unit
+public class AccountTest {// extends MockedSessionBeanFixture<AccountEJB> {
+
+	// public AccountTest() {
+	// super(AccountEJB.class);
+	// }
+	//
+	// public AccountTest(Class<AccountEJB> entityToTest) {
+	// super(entityToTest);
+	// // TODO Auto-generated constructor stub
+	// }
+	//
+	// public void testReverseStringAndAddDob() {
+	//
+	// /* define mocks/expectations */
+	// final Account accountMock = this.getMock(Account.class);
+	// accountMock.setFirstName("mockname");
+	// context.checking(new Expectations() {
+	// {
+	// one(accountMock).setFirstName("mockname");
+	// will(returnValue(accountMock.getFirstName()));
+	// }
+	// });
+	// /* run tests */
+	// AccountEJB account = this.getBeanToTest();
+	// Account result = account.saveAccount(accountMock);
+	//
+	// /* check result */
+	// final String expectedResult = "mockname";
+	// assertEquals(result, equals(expectedResult));
+	//
+	// }
+}
